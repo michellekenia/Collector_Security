326a7bf9a8d281ab668c80880eb2148cbaf0d29b
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-06 22:53:31 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/LeadCollectorSecurity/LeadCollectorSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.LeadCollectorSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadCollectorSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadCollectorSecurityApplication.class, args);
+	}
+
+}
